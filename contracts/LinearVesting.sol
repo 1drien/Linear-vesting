4700a6aab100acc9c6b1ba688cebdb7214bdb912
1drien <adrienmorlet2@gmail.com>
2023-06-18 18:12:22 +0200

    Add release function

--- contracts/LinearVesting.sol
+++ contracts/LinearVesting.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title LinearVesting
+ * @dev
+ */
 contract LinearVesting is Ownable {
     IERC20 token;
     uint256 public start; // quand le vesting commence
@@ -11,22 +15,15 @@ contract LinearVesting is Ownable {
     uint256 public duration; // durée du vesting
     address receiver; // destinataire
     uint256 amount; // nombre de tokkens envoyés
-    uint256 expiry;
-    bool public locked = false;
-    bool public claimed = false;
+    uint256 totalAmount; // montant total des tokens bloqués dans le contract
 
     //ajouter event
 
-    constructor(
-        address _token,
-        uint256 _start,
-        uint256 _duration,
-        uint256 _cliff
-    ) {
-        require(_duration > 0, "Duration must be positive");
+    constructor(address _token, uint256 _duration, uint256 _cliff) {
+        require(_duration > 0, "La duree doit etre positive");
         require(
             _duration >= _cliff,
-            "cliff ne peut être supérieur a la durée du vesting"
+            "cliff ne peut etre superieur a la duree du vesting"
         );
         cliff = _cliff;
         token = IERC20(_token);
@@ -34,32 +31,52 @@ contract LinearVesting is Ownable {
         duration = _duration;
     }
 
-    //imobiliser tokkens a periode determinée
+    /**
+     * @dev imobiliser tokkens a periode determinée
+     * @param _from description from
+     * @param _receiver description receiver
+     */
     function lock(
         address _from,
         address _receiver,
-        uint256 _amount,
-        uint256 _expiry
+        uint256 _amount
     ) public onlyOwner {
         // Vérifier que le montant à verrouiller est positif
-        require(_amount > 0, "Le montant à verrouiller doit être positif");
-        require(!locked, "We have alrady locked tokens");
-        /*require(totalAmount == 0, "Already locked");*/ // peut etre verrouillé qu'une seule fois
-        token.transferFrom(msg.sender, address(this), _amount);
+        require(_amount > 0, "Le montant a verrouiller doit etre positif");
+
+        require(totalAmount == 0, "Already locked"); // peut etre verrouillé qu'une seule fois
+
+        token.transferFrom(_from, address(this), _amount); //transférer les tokens du propriétaire original (_from) vers le contrat de vesting (address(this)).
 
         receiver = _receiver;
-        expiry = _expiry;
         amount = _amount;
         start = block.timestamp;
+        totalAmount = _amount;
     }
 
-    // libération des tokens selon letemps fixé
-
+    //Pas de paramètres
+    /**
+     * @dev libération linéaire des tokens selon letemps fixé
+     *
+     *
+     */
     function release() external {
-        require(!claimed, "tokens have been claimed");
-        require(block.timestamp >= start, "Vesting n'est pas commencé");
-        claimed = true;
-        //libération es tokens linéaire
-        token.transfer(reciver, amount);
+        require(block.timestamp >= start, "Vesting n'est pas commence");
+        require(
+            block.timestamp >= start + cliff,
+            "Avant le cliff rien ne peut etre retire"
+        ); //le délai de vesting a commencé.
+        require(
+            msg.sender == receiver || msg.sender == owner(),
+            "Vous n'etes pas autorise a liberer les tokens"
+        );
+        // calcule du pourcentage de temps depuis le debut du vesting
+        uint256 pourcentageTime = (block.timestamp - start) / duration;
+
+        //combien de tokens sont disponibles pour être libérés en fonction du temps écoulé depuis le début du vesting.
+        uint256 totalAmount = pourcentageTime * amount;
+        //Transférer ces tokens au bénéficiaire.
+
+        token.transfer(receiver, totalAmount);
     }
 }

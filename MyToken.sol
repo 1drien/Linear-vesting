5a35474e67a605c75578cfe77a11f848ad64ff31
1drien <adrienmorlet2@gmail.com>
2023-06-20 12:48:35 +0200

    added English comments

--- MyToken.sol
+++ MyToken.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
     constructor(uint256 initialSupply) public ERC20("MyToken", "MTK") {
-        _mint(msg.sender, initialSupply); // le nombre initial de tokens fonctio interne ERC20
+        _mint(msg.sender, initialSupply); // the initial number of tokens, internal function from ERC20
     }
 }

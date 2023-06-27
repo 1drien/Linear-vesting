85cd20de17a266219cab7cbb84fc7ad82de1adda
1drien <adrienmorlet2@gmail.com>
2023-06-27 10:32:40 +0200

    add test for lock function

--- tests/test_LinearVesting.py
+++ tests/test_LinearVesting.py
@@ -1,8 +1,21 @@
 from brownie import accounts
 
 
-def test_account_balance():
-    balance = accounts[0].balance()
-    accounts[0].transfer(accounts[1], "10 ether", gas_price=0)
+def test_lock():
+    # Arrange
+    settings = {"duration": 180, "cliff": 90, "amount": 1000}
+    account = accounts[0]
+    receiver = accounts[1]
+    token_address = "adresse_du_token"
+    accounts_param = {"from": account}
 
-    assert balance - "10 ether" == accounts[0].balance()
+    vesting_contract = LinearVesting.deploy(
+        token_address, settings["duration"], settings["cliff"], accounts_param
+    )
+
+    # Act
+    vesting_contract.lock(receiver, settings["amount"], accounts_param)
+
+    # Assert
+    assert vesting_contract.receiver() == receiver
+    assert vesting_contract.amount() == settings["amount"]
